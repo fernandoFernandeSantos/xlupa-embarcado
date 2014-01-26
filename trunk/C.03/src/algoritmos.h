3b5872d90a150ad66087f79d81732105e1267b9d
fernandofernandesantos@gmail.com <fernandofernandesantos@gmail.com@3b65f848-6576-f0bc-50ed-500555ae9acc>
2014-01-26 01:05:38 +0000

    Variaveis usadas diversas vezes em algoritmos.c são agora globais

--- trunk/C.03/src/algoritmos.h
+++ trunk/C.03/src/algoritmos.h
@@ -4,7 +4,7 @@
 #include <gtk/gtk.h>
 #include <string.h>
 void imagem_to_cinza(GdkPixbuf *subimage);
-void limiar_imagem(GdkPixbuf *subimage, guchar cor_rgb1[3], guchar cor_rgb2[3], int limite);
+void limiar_imagem(GdkPixbuf *subimage, guchar cor_rgb1[3], guchar cor_rgb2[3]); //antes tinha o limite
 void brilho_contraste_imagem(GdkPixbuf *subimage, double brilho, double contraste);
 #define PUT_PIXEL24(a,i,j,s,b) \
 (memcpy (a + ((j) * s + (i) * 3), &(b), 3))
