7bd2f6e60bf343b4c3b73e96b827b1c2e2ef5666
fernandofernandesantos@gmail.com <fernandofernandesantos@gmail.com@3b65f848-6576-f0bc-50ed-500555ae9acc>
2014-06-03 14:29:22 +0000

    0.1 da versão não sequencial

--- trunk/C.03/src/Headers/capture.h
+++ trunk/C.03/src/Headers/capture.h
@@ -7,14 +7,23 @@ int open_device(void);
 void init_device(void);
 void init_mmap(void);
 void start_capturing(void);
-int read_frame(void (*process_image)());
+int read_frame();
 void stop_capturing(void);
 void uninit_device (void);
 void close_device (void);
-void call_process_image(void (*process_image)(unsigned char*));
-//void main_loop();
+void * call_process_image();
+
+//TCC
+pthread_t threadProcess;
+pthread_t threadRead;
+
+unsigned int currentIndex;
+
+struct buffer * buffersV4L1;
+struct buffer * buffersV4L2;
 
 #define CLEAR(x) memset(&(x), 0, sizeof(x))
+#define BUFFERSIZE 5
 
 struct buffer {
     void * start;
