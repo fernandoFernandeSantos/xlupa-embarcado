fca2e1ae2e3622a8e6715bd51796fab777059fbb
fernandofernandesantos@gmail.com <fernandofernandesantos@gmail.com@3b65f848-6576-f0bc-50ed-500555ae9acc>
2014-09-22 22:59:07 +0000

    Novo xLupa Paralelo Valendo

---
+++ trunk/C.03/src/Main.c
@@ -0,0 +1,52 @@
+#include <gtk/gtk.h>
+#include "./Headers/ZoomWindow.h"
+#include "./Headers/capture.h"
+#include <unistd.h>  
+#include "./Headers/ZoomCanvas.h"
+#include "./Headers/debug.h"
+#include <string.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include "./Headers/clique.h"
+#include "./Headers/tableCreate.h"
+pthread_t clique;
+
+int main(int argc, char **argv) {
+    //chama a criação da tabela
+
+    //if (!naiveTable(vetR, vetG, vetB))
+    //    return -1;
+
+   // if (!expertTableCreate())
+   //     return -2;
+    /*printf("Loading expertTable...\n");
+    if(expertTableLoad())
+        printf("ExpertTable loaded!\n");
+    else
+        printf("Failed to load expertTable\n");
+    */
+    gtk_init(&argc, &argv);
+    /* Para color é necessário colocar qual cor quer
+     * Se R = 0, se G = 1, se B = 2
+     * Para brilho ou contraste coloca-se o nível de brilho que se quer
+     * o nível vai de 0 a 1
+     */
+    zoom = 0;
+    color = 0;
+    gray = 0;
+    brilho = 0;
+    contraste = 0;
+    strcpy(dev_name, "/dev/video0");
+    GtkWidget* window = zoom_window_new(WIDTH, HEIGHT, "xLupa Embedded");
+    gtk_widget_set_app_paintable(window, TRUE);
+    pthread_create(&clique, NULL, &get_clique, NULL);
+
+    GtkWidget *canvas = zoom_canvas_new();
+    gtk_container_add(GTK_CONTAINER(window), canvas);
+    gtk_widget_show_all(window);
+    gtk_main();
+    printf(" Finaliza ");
+     
+     //expertTableDestroy();
+     return (0);
+}
---
+++ trunk/C.03/src/util.c
@@ -0,0 +1,23 @@
+
+#include <sys/time.h>
+#include <time.h>
+#include "./Headers/util.h"
+#include <stdio.h>
+#define MILLION 1000000.0
+
+void register_time(){
+	clock_gettime(CLOCK_REALTIME, &clock_time);
+}
+
+double get_time_mili(){
+	struct timespec temp;
+	clock_gettime(CLOCK_REALTIME, &temp);
+	double v1 =  ((double)temp.tv_sec - (double)clock_time.tv_sec)*1000.;
+	double v2 =  ((double)temp.tv_nsec - (double)clock_time.tv_nsec)/MILLION;
+	double val = v1+v2;
+	clock_time.tv_sec = temp.tv_sec;
+	clock_time.tv_nsec = temp.tv_nsec;
+	return val;
+}
+
+
